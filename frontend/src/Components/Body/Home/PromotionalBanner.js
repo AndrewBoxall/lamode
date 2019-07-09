565dfe39858ad20f2f83c5b73f6d1e5de0e4ca5a
Andrew Boxall <42051071+AndrewBoxall@users.noreply.github.com>
2019-07-09 15:51:35 +1200

    Add files via upload

--- frontend/src/Components/Body/Home/PromotionalBanner.js
+++ frontend/src/Components/Body/Home/PromotionalBanner.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './PromotionalBanner.css';
 import { Link } from 'react-router-dom';
-import promo from '../../../images/promotional-banner.jpg';
+import promoBanner from '../../../images/landingpage-banner/promotional-banner.jpg';
 
 class PromotionalBanner extends Component {
   render() {
     return (
       <div id="promotional-banner">
-        <img src={promo} alt="LaMode on-going promotion"></img>
+        <img src={promoBanner} alt="LaMode on-going promotion"></img>
         <div className="promotional-banner-overlay">
           <div>THE NEW</div>
           <h3>CATALOG</h3>
